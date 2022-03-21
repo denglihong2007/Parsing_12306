286467f4f2bcae41ac1aacc87341a9d9ea3cb749
denglihong2007 <2639367181@qq.com>
2022-03-21 17:46:21 +0800

    2022.03.21 U49：更新安装代码。

--- setup.bat
+++ setup.bat
@@ -3,6 +3,7 @@ python-3.10.3-amd64.exe
 pip install -i https://pypi.tuna.tsinghua.edu.cn/simple pip -U
 pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
 pip install requests
+pip install pyqt5
 cd Timetable_new
 python setup.py build
 python setup.py sdist
